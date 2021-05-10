0bc032cedf5b6d21366194512085ae4d07618e81
faiza203 <fiazfaiza981@gmail.com>
2021-05-10 14:22:30 +0500

    Create correct calculator

--- script.js
+++ script.js
@@ -3,15 +3,49 @@ const formulaScreenEl = document.querySelector(".formulaScreen");
 
 let outPutScreen = "";
 let formulaScreen = "";
+let result = "";
 
 function changeOutPutScreen(e) {
-  outPutScreen += e.value;
+  if (result !== "") {
+    outPutScreen = e.value;
+    formulaScreen = e.value;
+    result = "";
+  } else {
+    outPutScreen += e.value;
+    formulaScreen += e.value;
+  }
   outPutScreenEl.value = outPutScreen;
+  formulaScreenEl.value = formulaScreen;
 }
 
 function changeFormulaScreen(e) {
-  formulaScreen += outPutScreen + e.value;
+  formulaScreen += e.value;
   outPutScreen = "";
   formulaScreenEl.value = formulaScreen;
-  outPutScreen.value = "";
+  outPutScreenEl.value = "";
+}
+
+function clearOutPut() {
+  formulaScreen = "";
+  outPutScreen = "0";
+  formulaScreenEl.value = formulaScreen;
+  outPutScreenEl.value = "0";
+}
+
+function getResult() {
+  result = eval(formulaScreen);
+  formulaScreenEl.value = `${formulaScreen} = ${result}`;
+}
+
+function checkResult() {
+  if (formulaScreen !== "") {
+    const regex = /[1-9]$/g;
+    if (regex.test(formulaScreen) !== true) {
+      while (regex.test(formulaScreen) !== true) {
+        formulaScreen = formulaScreen.slice(0, -1);
+      }
+    } else {
+      getResult();
+    }
+  }
 }
